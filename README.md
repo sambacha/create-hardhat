7df85ae1aa70a895a656e0ac545a344e69dec3cc
sam <sam@freighttrust.com>
2021-03-07 18:48:08 -0800

    build(init): hardhat scaffold

---
+++ README.md
@@ -0,0 +1,19 @@
+# [create-hardhat](#)
+
+```bash
+npx create-hardhat $PROJECT_NAME
+```
+
+## Overview
+
+- hardhat typescript
+
+## Extending
+
+```bash
+$ example
+```
+
+## License
+
+SPDX-License-Identifier: MIT
--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,23 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('joi');
+const dotenv = require("dotenv");
+const path = require("path");
+const Joi = require("joi");
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 // TODO
 // JS or TS
-// CI integration 
+// CI integration
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    SOLIDITY_VERSION: Joi.version().default('0.6.12'),
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
+    SOLIDITY_VERSION: Joi.version().default("0.6.12"),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
